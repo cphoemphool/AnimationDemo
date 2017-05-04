e7a683a699a2506d37a33445bf96e546168877e4
cphoemphoolsinch518 <cphoemphoolsinch518@PR19.hhs.win.fuhsd.org>
2017-05-04 13:45:36 -0700

    Add all files from Animation Demo
    Made Mario fall!

---
+++ src/Mario.java
@@ -0,0 +1,42 @@
+
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.*;
+import java.util.*;
+
+public class Mario extends MovingImage {
+
+	public static final int MARIO_WIDTH = 40;
+	public static final int MARIO_HEIGHT = 60;
+	
+	
+	private double xV, yY;
+	
+	
+	public Mario(int x, int y) {
+		super("mario.png", x, y, MARIO_WIDTH, MARIO_HEIGHT);
+		
+		xV = 0;
+		yY = 0;
+	}
+
+	// METHODS
+	public void walk(int dir) {
+		// WALK!
+	}
+
+	public void jump() {
+		// JUMP!
+	}
+
+	public void act(ArrayList<Shape> obstacles) {
+		// FINISH ME!
+		
+		yY += 0.75;
+		
+		super.y += 0.5 * yY;
+	}
+
+
+}
